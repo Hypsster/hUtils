d265b4f388f1bff631df71a82264f065a8586d27
Hypsster <work@hypsster.com>
2023-06-12 01:52:19 -0400

    Update ItemQuery.java

--- src/main/java/com/utils/HypsApiPlugin/ItemQuery.java
+++ src/main/java/com/utils/HypsApiPlugin/ItemQuery.java
@@ -18,72 +18,69 @@ import java.util.TreeSet;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class ItemQuery
-{
+public class ItemQuery {
 	private List<Widget> items;
 	static Client client = RuneLite.getInjector().getInstance(Client.class);
 	static ItemManager itemManager = RuneLite.getInjector().getInstance(ItemManager.class);
 
-	ItemQuery(List<Widget> items)
-	{
+	public ItemQuery(List<Widget> items) {
 		this.items = new ArrayList(items);
 	}
 
-	public ItemQuery filter(Predicate<? super Widget> predicate)
-	{
+	public ItemQuery filter(Predicate<? super Widget> predicate) {
 		items = items.stream().filter(predicate).collect(Collectors.toList());
 		return this;
 	}
 
-	public ItemQuery withAction(String action)
-	{
+	public ItemQuery withAction(String action) {
 		items = items.stream().filter(item -> Arrays.asList(item.getActions()).contains(action)).collect(Collectors.toList());
 		return this;
 	}
 
-	public ItemQuery withId(int id)
-	{
+	public ItemQuery withId(int id) {
 		items = items.stream().filter(item -> item.getItemId() == id).collect(Collectors.toList());
 		return this;
 	}
 
-	public ItemQuery withName(String name)
-	{
+	public ItemQuery withName(String name) {
 		items = items.stream().filter(item -> item.getName().equals(name)).collect(Collectors.toList());
 		return this;
 	}
 
-	public ItemQuery quantityGreaterThan(int quanity)
-	{
+	public ItemQuery quantityGreaterThan(int quanity) {
 		items = items.stream().filter(item -> item.getItemQuantity() > quanity).collect(Collectors.toList());
 		return this;
 	}
 
-	public ItemQuery nameContains(String name)
-	{
+	public ItemQuery nameContains(String name) {
 		items = items.stream().filter(item -> item.getName().contains(name)).collect(Collectors.toList());
 		return this;
 	}
 
-	public ItemQuery nonPlaceHolder()
-	{
+	public ItemQuery nonPlaceHolder() {
 		return quantityGreaterThan(0);
 	}
 
-	public ItemQuery idInList(List<Integer> ids)
-	{
+	public ItemQuery idInList(List<Integer> ids) {
 		items = items.stream().filter(item -> ids.contains(item.getItemId())).collect(Collectors.toList());
 		return this;
 	}
 
-	public ItemQuery indexIs(int index)
-	{
+	public ItemQuery nameInList(List<String> names) {
+		items = items.stream()
+				.filter(item -> names.stream()
+						.map(String::toLowerCase)
+						.anyMatch(name -> Text.removeTags(item.getName()).equalsIgnoreCase(name)))
+				.collect(Collectors.toList());
+		return this;
+	}
+
+	public ItemQuery indexIs(int index) {
 		items = items.stream().filter(item -> item.getIndex() == index).collect(Collectors.toList());
 		return this;
 	}
 
-	public ItemQuery matchesWildCardNoCase(String input)
-	{
+	public ItemQuery matchesWildCardNoCase(String input) {
 		items =
 				items.stream().
 						filter(item -> WildcardMatcher.matches(input.toLowerCase(), Text.removeTags(item.getName().toLowerCase()))).
@@ -91,41 +88,39 @@ public class ItemQuery
 		return this;
 	}
 
-	public ItemQuery onlyNoted()
-	{
+	public ItemQuery onlyNoted() {
 		items = items.stream().filter(this::isNoted).collect(Collectors.toList());
 		return this;
 	}
 
-	public boolean empty()
-	{
+	public ItemQuery onlyUnnoted() {
+		items = items.stream().filter(item -> !isNoted(item)).collect(Collectors.toList());
+		return this;
+	}
+
+	public boolean empty() {
 		return items.size() == 0;
 	}
 
-	public ItemQuery filterUnique()
-	{
+	public ItemQuery filterUnique() {
 		items = items.stream().collect(Collectors.collectingAndThen(Collectors.toCollection(() -> new TreeSet<>(Comparator.comparingInt(Widget::getItemId))), ArrayList::new));
 		return this;
 	}
 
-	public List<Widget> result()
-	{
+	public List<Widget> result() {
 		return items;
 	}
 
-	public Optional<Widget> first()
-	{
+	public Optional<Widget> first() {
 		Widget returnWidget = null;
-		if (items.size() == 0)
-		{
+		if (items.size() == 0) {
 			return Optional.ofNullable(null);
 		}
 		return Optional.ofNullable(items.get(0));
 	}
 
 	@SneakyThrows
-	public boolean isNoted(Widget item)
-	{
+	public boolean isNoted(Widget item) {
 		ItemComposition itemComposition = HypsApiPlugin.itemDefs.get(item.getItemId());
 		return itemComposition.getNote() != -1;
 	}
